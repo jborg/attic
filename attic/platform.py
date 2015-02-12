021fdf69c8a649bfa98234bb1cf5b5dad0a1df0c
Kristoffer Nordström <kristoffer.nordstroem@gmail.com>
2015-02-12 20:34:17 +0100

    First cygwin version, no ACL support though.

--- attic/platform.py
+++ attic/platform.py
@@ -8,6 +8,13 @@ elif platform == 'FreeBSD':
     from attic.platform_freebsd import acl_get, acl_set, API_VERSION
 elif platform == 'Darwin':
     from attic.platform_darwin import acl_get, acl_set, API_VERSION
+elif platform.startswith('CYGWIN'):
+    API_VERSION = 2
+
+    def acl_get(path, item, numeric_owner=False):
+        pass
+    def acl_set(path, item, numeric_owner=False):
+        pass
 else:
     API_VERSION = 1
 
