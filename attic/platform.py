f7552c83fa5b679fc8cb94e5ee2d450a077f04d6
Kristoffer Nordström <kristoffer.nordstroem@gmail.com>
2015-02-13 01:31:33 +0100

    Supporting ACL through (set|get)facl.exe system calls.

--- attic/platform.py
+++ attic/platform.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import io
 
 platform = os.uname()[0]
 
@@ -11,10 +13,33 @@ elif platform == 'Darwin':
 elif platform.startswith('CYGWIN'):
     API_VERSION = 2
 
-    def acl_get(path, item, numeric_owner=False):
-        pass
+    def acl_get(path, item, st, numeric_owner=False):
+        try:
+            # Using non-numeric names, i.e., group names, has caused problems.
+            # Hence the -n option
+            ACL_text = subprocess.check_output(['getfacl.exe', '-n', path])
+            item[b'acl_access'] = ACL_text
+        except:
+            pass
+
     def acl_set(path, item, numeric_owner=False):
-        pass
+        print ("acl_set()")
+        try:
+            ACL_Access = item[b'acl_access']
+        except:
+            return 
+        
+        buf = io.StringIO(ACL_Access.decode('utf-8'))
+
+        for line in buf:
+            if len(line.strip()) > 0 and (not line.strip().startswith('#')):
+                param = ['setfacl.exe', '-m', line.rstrip(), path]
+                #print(' '.join(param))
+                retVal = subprocess.call(param)
+                if retVal != 0:
+                    print(retVal)
+                    raise Exception('set ACL not successful')
+                                      
 else:
     API_VERSION = 1
 
